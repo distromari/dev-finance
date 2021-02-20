01075a7727ea89a66b2fee77f42d48e9b95a5448
Mari <mariana.morais@huia.haus>
2021-02-19 23:20:23 -0300

    Fixing some errors and updating readme

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# dev-finance
+<div id="readme" class="Box-body readme blob js-code-block-container">
+  <article class="markdown-body entry-content p-3 p-md-6" itemprop="text">
+    <p align="center">
+      <img alt="logo dev.finance$" src=".github/logo.png">
+    </p>
+    <p align="center">
+      <img alt="GitHub language count" src="https://img.shields.io/github/languages/count/distromari/dev-finance">
+      <img alt="GitHub top language" src="https://img.shields.io/github/languages/top/distromari/dev-finance">
+      <img alt="GitHub repo size" src="https://img.shields.io/github/repo-size/distromari/dev-finance">
+      <img alt="GitHub last commit" src="https://img.shields.io/github/last-commit/distromari/dev-finance">
+      <img alt="Github license" src="https://img.shields.io/github/license/distromari/dev-finance">
+    </p>
+    <p align="center"><img alt="landing dev.finance$" src=".github/devfinance.png"></p>
+    <h2>🎯 About</h2>
+    <p>dev.finance<span style="color: green">$</span> is an application for finance management. This project was developed at the <a href="https://maratonadiscover.rocketseat.com.br" rel="nofollow">Maratona Discover</a>, remembering and dominating programming and its ecosystem.</p>
+    <h2>🚀 Technologies</h2>
+    <p>This project was developed with the following technologies:</p>
+    <ul>
+      <li><a href="https://www.w3schools.com/html/" rel="nofollow">HTML</a></li>
+      <li><a href="https://www.w3schools.com/css/" rel="nofollow">CSS</a></li>
+      <li><a href="https://www.w3schools.com/js/" rel="nofollow">Javascript</a></li>
+    </ul>
+    <h2>🏁</a> Starting </h2>
+    <p>To clone and run this application, you'll need <a href="https://git-scm.com" rel="nofollow">Git</a>. From your command line:</p>
+    <div class="highlight highlight-source-shell">
+      <pre><span class="pl-c"><span class="pl-c">#</span> Clone this repository</span>
+$ git clone https://github.com/distromari/dev-finance
+<span class="pl-c"><span class="pl-c">#</span> Open index.html in your browser</span></pre>
+    </div>
+    <h2>📝 License</h2>
+    <p>This project is under the MIT license. See the <a href="./LICENSE" rel="nofollow">LICENSE</a> for more information.</p>
+  </article>
+</div>
--- scripts.js
+++ scripts.js
@@ -1,56 +1,34 @@
 const Modal = {
   open () {
-    document
-      .querySelector('.modal-overlay')
-      .classList
-      .add('active')
+    document.querySelector('.modal-overlay').classList.add('active')
   },
+
   close () {
-    document
-      .querySelector('.modal-overlay')
-      .classList
-      .remove('active')
+    document.querySelector('.modal-overlay').classList.remove('active')
   }
 }
 
 const Storage = {
   get() {
-    return JSON.parse(localStorage.getItem("dev.finances:transactions")) || []
+    return JSON.parse(localStorage.getItem("dev.finances:transactions")) || [];
   },
 
   set(transactions) {
-    localStorage.setItem("dev.finances:transactions", JSON.stringify(transactions))
+    localStorage.setItem("dev.finances:transactions", JSON.stringify(transactions));
   }
 }
 
 const Transaction = {
   all: Storage.get(),
-      add(transaction) {
-        Transaction.all.push(transaction)
 
-        App.reload()
-      },
-  // [
-  //   {
-  //     description: 'Light',
-  //     amount: -90,
-  //     date: '23/01/2021'
-  //   },
-  //   {
-  //     description: 'Website',
-  //     amount: 2000,
-  //     date: '23/01/2021'
-  //   },
-  //   {
-  //     description: 'Internet',
-  //     amount: -100,
-  //     date: '23/01/2021'
-  //   }
-  // ],
+  add(transaction) {
+    Transaction.all.push(transaction);
+    App.reload();
+  },
+
   remove(index) {
-    Transaction.all.splice(index, 1)
-
-    App.reload()
+    Transaction.all.splice(index, 1);
+    App.reload();
   },
 
   incomes() {
@@ -59,7 +37,7 @@ const Transaction = {
       if(transaction.amount > 0) {
         income += transaction.amount;
       }
-    })
+    });
     return income;
   },
 
@@ -69,29 +47,30 @@ const Transaction = {
       if(transaction.amount < 0) {
         expense += transaction.amount;
       }
-    })
+    });
     return expense;
   },
 
   total() {
     return Transaction.incomes() + Transaction.expenses();
-  }
-}
+  },
+};
 
 const DOM = {
   transactionsContainer: document.querySelector('#data-table tbody'),
+
   addTransaction(transaction, index) {
-    const tr = document.createElement('tr')
-    tr.innerHTML = DOM.innerHTMLTransaction(transaction, index)
-    tr.dataset.index = index
+    const tr = document.createElement('tr');
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction, index);
+    tr.dataset.index = index;
 
     DOM.transactionsContainer.appendChild(tr)
   },
 
   innerHTMLTransaction(transaction, index) {
-    const CSSclass = transaction.amount > 0 ? "income" : "expense"
+    const CSSclass = transaction.amount > 0 ? "income" : "expense";
 
-    const amount = Utils.formatCurrency(transaction.amount)
+    const amount = Utils.formatCurrency(transaction.amount);
 
     const html = `
       <td class="description">${transaction.description}</td>
@@ -100,48 +79,41 @@ const DOM = {
       <td>
         <img onclick="Transaction.remove(${index})" src="./assets//minus.svg" alt="Remove transaction">
       </td>
-    `
+    `;
 
-    return html
+    return html;
   },
 
   updateBalance() {
-    document
-      .getElementById('incomeDisplay')
-      .innerHTML = Utils.formatCurrency(Transaction.incomes())
-    document
-      .getElementById('expenseDisplay')
-      .innerHTML = Utils.formatCurrency(Transaction.expenses())
-    document
-      .getElementById('totalDisplay')
-      .innerHTML = Utils.formatCurrency(Transaction.total())
+    document.getElementById('incomeDisplay').innerHTML = Utils.formatCurrency(Transaction.incomes());
+    document.getElementById('expenseDisplay').innerHTML = Utils.formatCurrency(Transaction.expenses());
+    document.getElementById('totalDisplay').innerHTML = Utils.formatCurrency(Transaction.total());
   },
 
   clearTransactions() {
-    DOM.transactionsContainer.innerHTML = ""
-  }
-}
+    DOM.transactionsContainer.innerHTML = "";
+  },
+};
 
 const Utils = {
   formatCurrency(value){
-    const signal = Number(value) < 0 ? "-" : ""
+    const signal = Number(value) < 0 ? "-" : "";
 
-    value = String(value).replace(/\D/g, "")
+    value = String(value).replace(/\D/g, "");
 
-    value = Number(value) / 100
+    value = Number(value) / 100;
 
     value = value.toLocaleString("en-US", {
       style: "currency",
       currency: "USD"
-    })
+    });
 
-    return signal + value
+    return signal + value;
   },
 
   formatAmount(value) {
-    value = Number(value) * 100
-
-    return value
+    value = value * 100
+    return Math.round(value)
   },
 
   formatDate(date) {
@@ -152,15 +124,15 @@ const Utils = {
 
 const Form = {
   description: document.querySelector('input#description'),
-  description: document.querySelector('input#amount'),
-  description: document.querySelector('input#date'),
+  amount: document.querySelector('input#amount'),
+  date: document.querySelector('input#date'),
 
   getValues() {
     return {
       description: Form.description.value,
       amount: Form.amount.value,
       date: Form.date.value
-    }
+    };
   },
 
   validateFields() {
@@ -176,19 +148,16 @@ const Form = {
   },
 
   formatValues() {
-    let { description, amount, date } = Form.getValues()
-    amount = Utils.formatAmount(amount)
-    date = Utils.formatDate(date)
-
-    return {
-      description,
-      amount,
-      date,
-    }
+    let { description, amount, date } = Form.getValues();
+    amount = Utils.formatAmount(amount);
+    date = Utils.formatDate(date);
+    return { description, amount, date };
   },
 
   clearFields() {
-    Form.description.value = ""
+    Form.description.value = "";
+    Form.amount.value = "";
+    Form.date.value = "";
   },
 
   saveTransaction(transaction) {
@@ -202,32 +171,32 @@ const Form = {
       //Verify if all the informations were filled
       Form.validateFields()
       //Format the data to save
-      const transaction = Form.formatValues()
+      const transaction = Form.formatValues();
       //Save
       Transaction.add(transaction);
       //Delete Fields
-      Form.clearFields()
+      Form.clearFields();
       //Close modal
-      Modal.close()
+      Modal.close();
     } catch (error) {
       alert(error.message)
     }
-  } 
-}
+  },
+};
 
 const App = {
   init() {
-    Transaction.all.forEach(DOM.addTransaction)
+    Transaction.all.forEach(DOM.addTransaction);
 
-    DOM.updateBalance()
+    DOM.updateBalance();
 
-    Storage.set(Transaction.all)
+    Storage.set(Transaction.all);
   },
 
   reload() {
-    DOM.clearTransactions()
-    App.init()
+    DOM.clearTransactions();
+    App.init();
   },
-}
+};
 
-App.init()
+App.init();
